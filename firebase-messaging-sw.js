8da6d808cc22c54be41a9620355cbc2ec9f5953c
mattiaforgione <mattia4g12010@gmail.com>
2026-03-26 18:12:14 +0100

    Add files via upload

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAsEa2LDN-q0YcoXCnaAPnT3NmALkK_fjA",
+    authDomain: "myatsom-29431.firebaseapp.com",
+    projectId: "myatsom-29431",
+    storageBucket: "myatsom-29431.firebasestorage.app",
+    messagingSenderId: "550570346589",
+    appId: "1:550570346589:web:790e47cbb2079a501d25b4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
